7ec306ebc22fc6db113cfeda5c28c4e1d13960c3
Alexander Liu <avliu-um@github.com>
2023-12-12 11:26:05 -0500

    specify better input params, future-proof for decoupling, debug Yandex, better error logging

--- monitor_images.py
+++ monitor_images.py
@@ -1,12 +1,25 @@
 import os
 import traceback
-import time
 
 from selenium import webdriver
 from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+def monitor_images(platform, local_image_filename, driver):
+    if platform == 'google':
+        links = get_google(driver, local_image_filename)
+    elif platform == 'yandex':
+        links = get_yandex(driver, local_image_filename)
+    else:
+        print('crying')
+        raise NotImplementedError
+
+    print(f'links: {links}')
+
+    return links
 
 def get_driver():
     options = webdriver.FirefoxOptions()
@@ -21,7 +34,7 @@ def get_google(driver, photo_filename):
 
     # Get the RIS button
     ris_selector = 'svg[class="Gdd5U"]'
-    cameraicon = WebDriverWait(driver, 10).until(
+    cameraicon = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, ris_selector))
     )
     cameraicon.click()
@@ -29,17 +42,15 @@ def get_google(driver, photo_filename):
 
     # Send the photo file
     photo_upload_selector = "input[type = 'file']"
-    file_input = WebDriverWait(driver, 10).until(
+    file_input = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, photo_upload_selector))
     )
     file_input.send_keys(photo_filename)
     print(f'uploaded image')
 
-    time.sleep(30)
-
     # Click on "Find image source" --> Gives exact matches
     exact_matches_selector = 'div[class="ICt2Q"]'
-    WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, exact_matches_selector))
     )
     driver.find_element(By.CSS_SELECTOR, exact_matches_selector).click()
@@ -47,7 +58,7 @@ def get_google(driver, photo_filename):
 
     # Get links
     link_selector = 'li a'
-    WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, link_selector))
     )
     link_elems = driver.find_elements(By.CSS_SELECTOR, link_selector)
@@ -59,8 +70,8 @@ def get_google(driver, photo_filename):
 
 def get_yandex(driver, photo_filename):
     driver.get('https://yandex.com/images/')
-    photo_upload_selector = "input[type = 'file']"
-    file_input = WebDriverWait(driver, 10).until(
+    photo_upload_selector = "input[type = 'fileASDF']"
+    file_input = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, photo_upload_selector))
     )
     file_input.send_keys(photo_filename)
@@ -70,7 +81,7 @@ def get_yandex(driver, photo_filename):
     exact_match = True
     try:
         exact_matches_empty_selector = 'div[class="CbirOtherSizes-EmptyMessage"]'
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, exact_matches_empty_selector))
         )
         exact_match = False
@@ -80,7 +91,7 @@ def get_yandex(driver, photo_filename):
 
     if exact_match:
         link_selector = 'li[class="CbirSites-Item"] div[class="CbirSites-ItemTitle"] a'
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, link_selector))
         )
         link_elems = driver.find_elements(By.CSS_SELECTOR, link_selector)
--- runner.py
+++ runner.py
@@ -1,76 +1,90 @@
-import csv
 import os
+import traceback
 import boto3
 import json
 from datetime import datetime
-from monitor_images import get_driver, get_google, get_yandex  # Importing the needed function from your module.
 
+from monitor_images import get_driver, monitor_images
 
-def run(params_list: list):
+from selenium.common import WebDriverException
+
+def run(platforms: list, input_s3_bucket: str, input_s3_filenames: list, output_s3_bucket: str):
+    os.makedirs('tmp', exist_ok=True)
     s3_client = boto3.client('s3')
-
     driver = get_driver()
-    old_s3_bucket_name = ''
-    s3_old_image_filename = ''
 
-    local_image_filename = 'image.jpg'
-    local_output_filename = 'output.json'
+    local_image_filename = f'{os.getcwd()}/tmp/image.jpg'
+    local_output_filename = 'tmp/output.json'
+    local_error_output_filename = 'tmp/error.html'
 
-    for params in params_list:
-        platform = params['platform']
-        s3_bucket_name = params['s3_bucket_name']
-        s3_image_filename = params['s3_image_filename']
-
-        # doanload file locally
-        if s3_image_filename != s3_old_image_filename or s3_bucket_name != old_s3_bucket_name:
+    for input_s3_filename in input_s3_filenames:
+        if os.path.isfile(local_image_filename):
             os.remove(local_image_filename)
-            s3_client.download_file(s3_bucket_name, s3_image_filename, local_image_filename)
+        s3_client.download_file(input_s3_bucket, input_s3_filename, local_image_filename)
 
-        if platform == 'google':
-            links = get_google(driver, local_image_filename)
-        elif platform == 'yandex':
-            links = get_yandex(driver, local_image_filename)
-        else:
-            print('crying')
+        for platform in platforms:
+            # Prepare S3 path with current date
+            today = datetime.strftime(datetime.now(), "%Y-%m-%d")
+            output_s3_filename = f'raw/{today}/{platform}_{input_s3_filename.split(".")[0].split("/")[-1]}_links.csv'
+            error_output_s3_filename = f'errors/{output_s3_filename}'
 
-        data = []
-        for link in links:
-            data.append({
-                'link': link,
-                'platform': platform,
-                'search_bucket': s3_bucket_name,
-                'search_image_filename': s3_image_filename
-            })
-        with open(local_output_filename, 'w') as fp:
-            json.dump(data, fp, indent=4, separators=(',', ': '))
+            print(f''
+                  f'platform: {platform}, '
+                  f'input s3 bucket name: {input_s3_bucket}, '
+                  f'input s3 image filename: {input_s3_filename}, '
+                  f'output s3 bucket name: {output_s3_bucket}, '
+                  f'output s3 filename: {output_s3_filename}'
+                  )
 
-        # Prepare S3 path with current date
-        today = datetime.today()
-        s3_output_path = f'raw/{today.isoformat()}/{platform}_{s3_image_filename.split("/")[-1]}_links.csv'
-        # Upload output file to s3
-        s3_client.upload_file(local_output_filename, s3_bucket_name, s3_output_path)
+            try:
+                links = monitor_images(platform, local_image_filename, driver)
+
+                data = []
+                for link in links:
+                    data.append({
+                        'link': link,
+                        'platform': platform,
+                        'search_bucket': input_s3_bucket,
+                        'search_image_filename': input_s3_filename
+                    })
+                with open(local_output_filename, 'w') as fp:
+                    json.dump(data, fp, indent=4, separators=(',', ': '))
+
+                # Upload output file to s3
+                s3_client.upload_file(local_output_filename, output_s3_bucket, output_s3_filename)
+
+            except WebDriverException as e:
+                print(f'selenium error! ')
+                print(str(e))
+                print('stack trace:')
+                traceback.print_exc()
+
+                print(f'writing error file to {error_output_s3_filename}')
+                with open(local_error_output_filename, "w", encoding='utf-8') as f:
+                    f.write(driver.page_source)
+                s3_client.upload_file(local_error_output_filename, output_s3_bucket, error_output_s3_filename)
+
+            print()
 
 
 def main():
-    s3_bucket_name = os.getenv('s3_bucket_name')
-    s3_client = boto3.client('s3')
+    platforms = ['google', 'yandex']
+    input_s3_bucket = os.getenv('input_s3_bucket')
+    input_s3_files = os.getenv('input_s3_files')
+    output_s3_bucket = os.getenv('output_s3_bucket')
 
-    params = []
+    if input_s3_files is None:
+        input_s3_files = []
+        s3_client = boto3.client('s3')
+        paginator = s3_client.get_paginator('list_objects')
+        for result in paginator.paginate(Bucket=input_s3_bucket, Prefix='images'):
+            for file in result['Contents']:
+                s3_image_filename = file['Key']
+                input_s3_files.append(s3_image_filename)
+    else:
+        input_s3_files = input_s3_files.split(',')
 
-    paginator = s3_client.get_paginator('list_objects')
-    for result in paginator.paginate(Bucket=s3_bucket_name, Prefix='images'):
-        for file in result['Contents']:
-            s3_image_filename = file['Key']
-
-            for platform in ['google', 'yandex']:
-                params.append({
-                    'platform': platform,
-                    's3_bucket_name': s3_bucket_name,
-                    's3_image_filename': s3_image_filename
-                })
-
-    run(params)
-
+    run(platforms, input_s3_bucket, input_s3_files, output_s3_bucket)
 
 
 if __name__ == "__main__":
